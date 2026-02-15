e7b3b7d37c3b12242fc4b2bdc5058bc2fd31521d
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-02-15 01:02:07 -0400

    feat: initialize Name property in Category class with default value

--- backend/Expenses.Entities/Models/Category.cs
+++ backend/Expenses.Entities/Models/Category.cs
@@ -5,7 +5,7 @@ public class Category
     // Guid porque es el identificador mas escalable y facil de operar
     // Id es init porque no se podra cambiar una vez creado el objeto, ya que el Id es la clave primaria y no deberia cambiarse
     public Guid Id { get; init; }
-    public required string Name { get; set; }
+    public required string Name { get; set; } = string.Empty;
     public decimal MonthlyBudget { get; set; }
 
     // IconKey sera la clave para identificar el icono en el frontend, idk por ahora lo dejare como "DefaultIcon" hasta que haga el frontend y vea como manejar los iconos
