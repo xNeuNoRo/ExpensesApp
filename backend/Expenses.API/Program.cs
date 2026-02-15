6baa0117781b5df4d819f1e398fd77e700107353
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-02-15 19:41:07 -0400

    feat: refactor Program.cs to enhance middleware integration and streamline service registration

--- backend/Expenses.API/Program.cs
+++ backend/Expenses.API/Program.cs
@@ -1,41 +1,39 @@
+using Expenses.API.Middlewares;
+using Expenses.BLL;
+using Expenses.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Registramos los servicios necesarios para el funcionamiento de la aplicación, como controladores y OpenAPI (Swagger) para documentación de la API
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+// Inyectamos las capas personalizadas que hemos creado previamente
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddBusinessServices();
+
+// Construimos la aplicación a partir de la configuración y servicios registrados
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuramos el pipeline de procesamiento de las solicitudes HTTP
+
+// Aqui agregamos nuestro middleware personalizado para manejar excepciones de manera centralizada en toda la aplicación
+app.UseMiddleware<ExceptionMiddleware>();
+
+// Si estamos en desarrollo, habilitamos la documentación de la API con Swagger/OpenAPI
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+// Redirigimos todas las solicitudes HTTP a HTTPS para mayor seguridad
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Habilitamos la autorización, aunque en este punto no hemos configurado nada relacionado a la autorización, es una buena práctica tenerlo en el pipeline (por si acaso) por si se necesita en el futuro
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Mapeamos los controladores para que puedan manejar las solicitudes HTTP entrantes
+app.MapControllers();
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// Finalmente, ejecutamos la aplicación, lo que pone en marcha el servidor web y comienza a escuchar las solicitudes HTTP
+await app.RunAsync();
